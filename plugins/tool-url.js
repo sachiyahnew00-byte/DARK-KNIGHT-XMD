cffa1ff519eed4f7e788f84bf3aa2f7776316256
bot-deploy-main <152987682+bot-deploy-main@users.noreply.github.com>
2026-05-25 22:39:18 +0530

    Update tool-url.js

--- plugins/tool-url.js
+++ plugins/tool-url.js
@@ -46,12 +46,11 @@ cmd({
      const response = await axios.post("https://catbox.moe/user/api.php", form, {
       headers: {
         ...form.getHeaders(),
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        "Accept-Language": "en-US,en;q=0.9",
-        "Origin": "https://catbox.moe",
-        "Referer": "https://catbox.moe/",
-        "Sec-Ch-Ua": '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"'
+         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+         "Accept": "*/*",
+         "Accept-Language": "en-US,en;q=0.9",
+         "Origin": "https://catbox.moe",
+         "Referer": "https://catbox.moe/",
       }
     });
     
